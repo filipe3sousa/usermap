c4574cf4af6f92f3b72cdc810a225beb01315654
Filipe Sousa <filipesousa@parceiro.sonae.pt>
2017-08-10 12:23:00 +0100

    get the users from UserService and populate a list with those users;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  constructor(
+    private userService: UserService
+  ) {}
+
+  users: any[];
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe( res => {
+      this.users = res
+    });
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
 }
